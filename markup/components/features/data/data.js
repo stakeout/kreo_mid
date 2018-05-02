fdd53c91e59c415881e1f0ef47a19655571f56ad
Vadim Novash <novash@tut.by>
2018-05-02 19:34:38 +0300

    features block with conditions

---
+++ markup/components/features/data/data.js
@@ -0,0 +1,103 @@
+features: {
+  kreoView: [
+    {
+      title: 'Export Revit model to Kreo View instantly',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Collaborate with your team members and clients',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Demo your BIM models',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Real time collision detection',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'View all BIM model information attributes',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Share your BIM model',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Use Clip-box option to examine the BIM-model',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Walk through the BIM-model',
+      url: 'http://placehold.it/350x185'
+    }
+  ],
+  kreoPlan: [
+    {
+      title: 'Export Revit model to Kreo View instantly',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/k_GNhW5nQHo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        title: 'Collaborate with your team members and clients',
+        list: [
+          {
+            item: 'row 1',
+          },
+          {
+            item: 'row 2',
+          },
+          {
+            item: 'row 3',
+          },
+          {
+            item: 'row 4',
+          }
+        ]
+      }
+    },
+    {
+      title: 'Collaborate with your team members and clients',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/k_GNhW5nQHo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        list: [
+          {
+            item: 'row 1',
+          },
+          {
+            item: 'row 2',
+          },
+          {
+            item: 'row 3',
+          },
+          {
+            item: 'row 4',
+          }
+        ]
+      }
+    },
+    {
+      title: 'Demo your BIM models',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Real time collision detection',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'View all BIM model information attributes',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Share your BIM model',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Use Clip-box option to examine the BIM-model',
+      url: 'http://placehold.it/350x185'
+    },
+    {
+      title: 'Walk through the BIM-model',
+      url: 'http://placehold.it/350x185'
+    }
+  ]
+}
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,11 +1,11 @@
 'use strict';
 import {stickyNav} from '../../components/header/header';
 import  * as tabs from '../../components/solutions/solutions';
+// import  * as youtube from '../../components/features/features';
 /*
     This file can be used as entry point for webpack!
  */
 $(() => {
   stickyNav();
   $(".solutions__inner").tabs();
-
 });
