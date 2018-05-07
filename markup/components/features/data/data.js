bb755f081aede6a1dd751ba067e0680e1daeaa92
Vadim Novash <novash@tut.by>
2018-05-07 18:36:22 +0300

    add content to features + link revit

--- markup/components/features/data/data.js
+++ markup/components/features/data/data.js
@@ -1,42 +1,185 @@
 features: {
+  kreoPlan: [
+    {
+      title: 'BIM Model Validation',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/JZ9Lvl1wGp4" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        title: 'KREO uses machine learning to identify and classify BIM model elements. It takes into account both the geometry of a BIM element and its other attributes, ensuring the highest accuracy of the classification. With KREO you can be sure that all BIM elements are accounted for.',
+        list: [
+          {
+            item: 'AI-powered BIM elements’ classification under <a href="https://www.thenbs.com/services/our-tools/introducing-uniclass-2015">Uniclass</a> or <a href="https://www.csiresources.org/practice/standards/masterformat">MasterFormat</a>',
+          },
+          {
+            item: 'BIM model check  to identify issues affecting constructability and measurement accuracy'
+          }
+        ]
+      }
+    },
+    {
+      title: 'BIM Quantity Take-off',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/SZJgGgrxr-s" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        title: 'KREO generates detailed bills of quantities by default. It performs quantity take-off for each BIM element according to the units of measurement specified in the model and follows the NRM requirements',
+        list: [
+          {
+            item: 'Full BIM element count',
+          },
+          {
+            item: 'Automated measurements under <a href="http://www.rics.org/uk/knowledge/professional-guidance/guidance-notes/new-rules-of-measurement-order-of-cost-estimating-and-elemental-cost-planning/">New Rules of Measurement</a>',
+          },
+          {
+            item: '3D visualisation of BIM elements to audit quantities and measurements',
+          },
+          {
+            item: 'Automated generation of Bills of Quantities (BoQs)'
+          }
+        ]
+      }
+    },
+    {
+      title: 'BIM Cost Estimation',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/Veq8F3BXRho" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        title: 'KREO automatically generates the cost estimate of a BIM construction project. It uses AI to assign activities and resources to each BIM element. KREO provides full integration with leading cost data providers - <a href="http://www.rics.org/uk/knowledge/bcis/">BCIS</a> in the UK or <a href="https://www.rsmeans.com/products/online.aspx">RS Means</a> in the US and Canada. You can also use your internal cost database. The estimate provides a detailed breakdown of costs for each activity (labour, plant and materials). KREO takes into consideration the following factors that influence a cost budget:',
+        list: [
+          {
+            item: 'Quantitative and qualitative gang composition',
+          },
+          {
+            item: 'Site congestion and overstaffing',
+          },
+          {
+            item: 'Resource limitations',
+          },
+          {
+            item: 'The sequence of activities',
+          },
+          {
+            item: 'Work packaging.'
+          }
+        ]
+      }
+    },
+    {
+      title: '4D Scheduling',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/OoZ3s8fjcyQ" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        title: 'KREO developed an AI-based optimisation and scheduling engine to create a project master schedule. It runs a large number of simulations to find an optimal balance between cost and time. To achieve this balance KREO considers the following factors:',
+        list: [
+          {
+            item: 'Built-in sequencing rules',
+          },
+          {
+            item: 'User-defined resource limits',
+          },
+          {
+            item: 'Resource utilisation level',
+          },
+          {
+            item: 'Activity flow by work package',
+          },
+          {
+            item: 'Spatial limitations.'
+          }
+
+        ]
+      }
+    },
+    {
+      title: '\'What-if\' Scenarios',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/8WmR3Ru9q68" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        title: 'Create a whole range of scenarios for a project schedule and cost. Analyse significant cost and duration drivers. Test your assumption in a matter of minutes. <br><br> A side-by-side scenario comparison is a powerful tool for:',
+        list: [
+          {
+            item: 'Cost planning ',
+          },
+          {
+            item: 'Risk analysis',
+          },
+          {
+            item: 'Value engineering',
+          },
+          {
+            item: 'Pre-tender estimating'
+          }
+        ]
+      }
+    },
+    {
+      title: 'BIM-based Tendering & Bid Pricing',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/57i2FWnmgsQ" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        title: 'KREO developed a unique platform for online tendering and bidding for BIM-based projects. With KREO, bid managers can price bids and get pre-tender estimates within days. KREO breaks the whole project into work packages by default. It groups activities by trade and ensures their uninterrupted flow. You can adjust package size and composition without much effort. <br><br> Once you are happy with work packages, you can use KREO\'s online platform to launch RFQs, RFPs and tenders. You can invite bidders by email. After registration and authentication, bidders can access their package information online. They have access to detailed bills of quantities, a project schedule and 4D model.<br><br> The bidders can submit their bids online by filling in online forms. Following the submission, KREO compares the bids and ranks them. You can shortlist and nominate winners and notify them online. Finally, you can check the impact of the selected bids on the project master schedule and cost budget.',
+        list: [
+          {
+            item: 'Automated work packaging',
+          },
+          {
+            item: 'Automated generation of BoQs, cost estimates and schedules by work package',
+          },
+          {
+            item: 'Invite subcontractors to online project portal',
+          },
+          {
+            item: 'Provide each bidder with the bill of quantities and schedule with 4D visualisation',
+          },
+          {
+            item: 'Allow subcontractors to submit their bids online',
+          },
+          {
+            item: 'Compare all submitted bids online and select the best one in each package',
+          },
+          {
+            item: 'Update master schedule and cost estimate to incorporate winning bids'
+          }
+        ]
+      }
+    },
+    {
+      title: 'Dashboard with Analytics',
+      bg: '__static__img/general/dashboard_with_analytics.png',
+      hint: {
+        title: 'KREO brings all the essential information relevant to a project to interactive dashboards.  A visual presentation of the data helps to identify areas requiring further analysis. It also makes communication with key stakeholders and decision-makers much easier.',
+        list: [
+          {
+            item: 'Visualise key cost and schedule drivers of the project',
+          },
+          {
+            item: 'Compare scenarios',
+          },
+          {
+            item: 'Review resource utilisation',
+          },
+          {
+            item: 'Improve communication with key stakeholders with a powerful reporting tool',
+          }
+        ]
+      }
+    },
+    {
+      title: 'Knowledge Base & Database Management Tools',
+      bg: '__static__img/general/kb_and_dbmanagement_tools.png',
+      hint: {
+        title: 'Whether it is a new construction method, sequence logic or a business rule, KREO can help you capture this knowledge and re-use it. With each new BIM project, the accumulated knowledge improves the speed and quality of the outputs.<br><br> This combination of expert knowledge and machine intelligence has the potential to transform your business',
+        list: [
+          {
+            item: 'Capture and reuse experts knowledge',
+          },
+          {
+            item: 'Create custom construction methods, sequencing logic, resource requirements, productivity drivers',
+          },
+          {
+            item: 'Share your knowledge within your company',
+          }
+        ]
+      }
+    }
+  ],
   kreoView: [
     {
       title: 'Export Revit model to Kreo View instantly',
-      url: 'http://placehold.it/350x185'
-    },
-    {
-      title: 'Collaborate with your team members and clients',
-      url: 'http://placehold.it/350x185'
-    },
-    {
-      title: 'Demo your BIM models',
-      url: 'http://placehold.it/350x185'
-    },
-    {
-      title: 'Real time collision detection',
-      url: 'http://placehold.it/350x185'
-    },
-    {
-      title: 'View all BIM model information attributes',
-      url: 'http://placehold.it/350x185'
-    },
-    {
-      title: 'Share your BIM model',
-      url: 'http://placehold.it/350x185'
-    },
-    {
-      title: 'Use Clip-box option to examine the BIM-model',
-      url: 'http://placehold.it/350x185'
-    },
-    {
-      title: 'Walk through the BIM-model',
-      url: 'http://placehold.it/350x185'
-    }
-  ],
-  kreoPlan: [
-    {
-      title: 'Export Revit model to Kreo View instantly',
-      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/k_GNhW5nQHo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/g7C9-OCVGzk?start=10&end=50&versoin=3" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
       hint: {
         title: 'Collaborate with your team members and clients',
         list: [
@@ -57,7 +200,7 @@ features: {
     },
     {
       title: 'Collaborate with your team members and clients',
-      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/k_GNhW5nQHo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/g7C9-OCVGzk?start=240&end=281&versoin=3"  allow="autoplay; encrypted-media" allowfullscreen></iframe>',
       hint: {
         list: [
           {
@@ -77,27 +220,123 @@ features: {
     },
     {
       title: 'Demo your BIM models',
-      url: 'http://placehold.it/350x185'
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/g7C9-OCVGzk?start=289&end=324&versoin=3"  allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        list: [
+          {
+            item: 'row 1',
+          },
+          {
+            item: 'row 2',
+          },
+          {
+            item: 'row 3',
+          },
+          {
+            item: 'row 4',
+          }
+        ]
+      }
     },
     {
       title: 'Real time collision detection',
-      url: 'http://placehold.it/350x185'
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/g7C9-OCVGzk?start=163&end=239&versoin=3"  allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        list: [
+          {
+            item: 'row 1',
+          },
+          {
+            item: 'row 2',
+          },
+          {
+            item: 'row 3',
+          },
+          {
+            item: 'row 4',
+          }
+        ]
+      }
     },
     {
       title: 'View all BIM model information attributes',
-      url: 'http://placehold.it/350x185'
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/LdgUkv00hOs" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        list: [
+          {
+            item: 'row 1',
+          },
+          {
+            item: 'row 2',
+          },
+          {
+            item: 'row 3',
+          },
+          {
+            item: 'row 4',
+          }
+        ]
+      }
     },
     {
       title: 'Share your BIM model',
-      url: 'http://placehold.it/350x185'
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/g7C9-OCVGzk?start=127&end=159&versoin=3"  allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        list: [
+          {
+            item: 'row 1',
+          },
+          {
+            item: 'row 2',
+          },
+          {
+            item: 'row 3',
+          },
+          {
+            item: 'row 4',
+          }
+        ]
+      }
     },
     {
       title: 'Use Clip-box option to examine the BIM-model',
-      url: 'http://placehold.it/350x185'
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/RoBgLTEkde4" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        list: [
+          {
+            item: 'row 1',
+          },
+          {
+            item: 'row 2',
+          },
+          {
+            item: 'row 3',
+          },
+          {
+            item: 'row 4',
+          }
+        ]
+      }
     },
     {
       title: 'Walk through the BIM-model',
-      url: 'http://placehold.it/350x185'
+      url: '<iframe width="280" height="148" src="https://www.youtube.com/embed/-QKicxBMj5E" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+      hint: {
+        list: [
+          {
+            item: 'row 1',
+          },
+          {
+            item: 'row 2',
+          },
+          {
+            item: 'row 3',
+          },
+          {
+            item: 'row 4',
+          }
+        ]
+      }
     }
   ]
 }
--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -1,9 +1,11 @@
 'use strict';
+// import magnificPopup from 'magnific-popup';
 import {stickyNav} from '../../components/header/header';
 import  * as tabs from '../../components/solutions/solutions';
 // import  * as youtube from '../../components/features/features';
 import  * as faq from '../../components/faq/faq';
 import iziModal from 'iziModal-1.6.0/js/iziModal';
+// import * as magnificPopup from '../../components/features/magnific';
 /*
     This file can be used as entry point for webpack!
  */
@@ -20,12 +22,4 @@ $(() => {
       closeButton: true
     });
   }
-  var kreoView = $('#kreoView');
-  if(kreoView.length) {
-    kreoView.iziModal({
-      width: '600px',
-      closeOnEscape: true,
-      closeButton: true
-    });
-  }
-});
+})
