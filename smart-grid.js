7f15faff75758a202a4817c9da02000c38a1ac9c
Vadim Novash <novash@tut.by>
2018-05-19 13:53:52 +0300

    992px adaptive without promo block

--- smart-grid.js
+++ smart-grid.js
@@ -9,24 +9,19 @@ const settings = {
     },
     breakPoints: {
         lg: {
-            width: '992px',
-            fields: '15px'
+            width: '992px'
         },
         md: {
-            width: '768px',
-            fields: '15px'
+            width: '768px'
         },
         sm: {
-            width: '576px',
-            fields: '15px'
+            width: '576px'
         },
         xs: {
-            width: '480px',
-            fields: '15px'
+            width: '480px'
         },
         xxs: {
-            width: '320px',
-            fields: '15px'
+            width: '320px'
         }
     },
     oldSizeStyle: false,
