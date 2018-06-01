f0a8d2df1545cda006a7508f63c3e8f9de271351
Vadim Novash <novash@tut.by>
2018-06-01 16:50:05 +0300

    fixes in styles overview + automatation

--- markup/components/blog-data-list/blog-data-list.js
+++ markup/components/blog-data-list/blog-data-list.js
@@ -1,6 +1,6 @@
 export const blogSubscription = () => {
   const subscribeWrap = $('<li class="blog-data__item subscribe-to"><p>subscribe to our newsletter</p><a class="btn btn--orange" href="#email-fec0ddd3-8f7b-4745-9f57-2fce8053c979">subscribe</a></li>');
-  const blogItem = $('.blog-data__item:nth-child(2)');
+  const blogItem = $('.blog-data__item:nth-child(1)');
   if (subscribeWrap) {
     console.log(subscribeWrap);
     blogItem.after(subscribeWrap);
