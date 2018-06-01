c253d21e617c2ee01febc222c48e374fdda3fdfc
Vadim Novash <novash@tut.by>
2018-06-01 15:53:06 +0300

    overview + automatation

--- smart-grid.js
+++ smart-grid.js
@@ -4,7 +4,7 @@ const settings = {
     columns: 12,
     offset: '30px',
     container: {
-        maxWidth: '1200px',
+        maxWidth: '1320px',
         fields: '30px'
     },
     breakPoints: {
